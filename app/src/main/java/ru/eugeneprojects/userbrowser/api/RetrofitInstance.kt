65769649ac3b9b43f1290c4323f81b7e59bac78c
EugeneGridnev <eugenegrid@yandex.ru>
2024-03-16 20:37:39 +0300

    utils added

--- app/src/main/java/ru/eugeneprojects/userbrowser/api/RetrofitInstance.kt
+++ app/src/main/java/ru/eugeneprojects/userbrowser/api/RetrofitInstance.kt
@@ -2,12 +2,13 @@ package ru.eugeneprojects.userbrowser.api
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import ru.eugeneprojects.userbrowser.util.Constants
 
 object RetrofitInstance {
 
     val api:UserAPI by lazy {
         Retrofit.Builder()
-            .baseUrl("https://randomuser.me")
+            .baseUrl(Constants.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(UserAPI::class.java)
---
+++ app/src/main/java/ru/eugeneprojects/userbrowser/util/Constants.kt
@@ -0,0 +1,7 @@
+package ru.eugeneprojects.userbrowser.util
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://randomuser.me"
+    }
+}
